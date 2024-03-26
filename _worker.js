b89b93b0d7ebd2565b30632ed680558cf85ea426
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-03-27 04:47:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,10 +5,10 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
 let addresses = [
-	'dn1.wo108.dedyn.io:2053#onlyno999-香港',
-	'dn2.wo108.dedyn.io:2053#域名解析-香港',
+	'dn1.wo108.dedyn.io:8443#onlyno999-香港',
+	'dn2.wo108.dedyn.io:8443#域名解析-香港',
 	'dn3.wo108.dedyn.io:2096#阿里域名-香港',
-	'dn4.wo108.dedyn.io:2096#阿里域名-香港',
+	'dn4.wo108.dedyn.io:8443#阿里域名-香港',
 	'dn5.wo108.dedyn.io:8443#阿里域名-香港',
 	'dn6.wo108.dedyn.io:8443#阿里域名-香港',
 	'dn7.wo108.dedyn.io:8443#阿里域名-香港',
